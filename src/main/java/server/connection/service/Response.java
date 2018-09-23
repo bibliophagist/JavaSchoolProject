0e0d8645343128876b1c5f8733bf7d25917f171e
Senex626 <feelshadow5@gmail.com>
2018-09-23 23:19:34 +0300

    Changes to Response class and AppCore

--- src/main/java/server/connection/service/Response.java
+++ src/main/java/server/connection/service/Response.java
@@ -1,9 +1,15 @@
-package server.connection.service;
+package sber.jpadb;
+
+import java.util.List;
+
+import sber.model.Account;
 
 public class Response {
 
-	private String responseMessage = "Success";
-	private boolean requestSuccessful = true;
+	private String responseMessage = "Initial message";
+	private boolean requestSuccessful = false;
+	private String userID;
+	private List<Account> accList;
 	
 	public Response() {
 	}
@@ -27,5 +33,21 @@ public class Response {
 	
 	public void setRequestSuccessful(boolean successStatus) {
 		this.requestSuccessful = successStatus;
+	}
+	
+	public List<Account> getAcclist() {
+		return accList;
+	}
+	
+	public void setAccList(List<Account> accList) {
+		this.accList = accList;
+	}
+	
+	public String getUserID() {
+		return userID;
+	}
+	
+	public void setUserID(String userID) {
+		this.userID = userID;
 	}
 }
